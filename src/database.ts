24b064a221059bdcbd2232e6c9c0703206864eb7
Toan Nguyen <nguyenvantoan263@gmail.com>
2023-10-21 23:39:16 +0800

    create env file and dataabse file

---
+++ src/database.ts
@@ -0,0 +1,33 @@
+import dotenv from 'dotenv';
+import { Pool } from 'pg';
+
+const {
+    POSTGRES_HOST,
+    POSTGRES_DB,
+    POSTGRES_USER,
+    POSTGRES_PASSWORD,
+    POSTGRES_TEST_DB,
+    ENV
+} = process.env;
+
+let client;
+
+if (ENV === 'PROD') {
+    client = new Pool({
+      host: POSTGRES_HOST,
+      database: POSTGRES_DB,
+      user: POSTGRES_USER,
+      password: POSTGRES_PASSWORD,
+    });
+  }
+  
+  if (ENV === 'TEST') {
+    client = new Pool({
+      host: POSTGRES_HOST,
+      database: POSTGRES_TEST_DB,
+      user: POSTGRES_USER,
+      password: POSTGRES_PASSWORD,
+    });
+  }
+
+  export default client;
