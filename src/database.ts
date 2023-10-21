5112413bd129344c31133e9c102866cd144a4e19
Toan Nguyen <nguyenvantoan263@gmail.com>
2023-10-22 00:52:20 +0800

    added dev environment in database

--- src/database.ts
+++ src/database.ts
@@ -7,6 +7,7 @@ const {
     POSTGRES_USER,
     POSTGRES_PASSWORD,
     POSTGRES_TEST_DB,
+    POSTGRES_DEV_DB,
     ENV
 } = process.env;
 
@@ -14,20 +15,30 @@ let client;
 
 if (ENV === 'PROD') {
     client = new Pool({
-      host: POSTGRES_HOST,
-      database: POSTGRES_DB,
-      user: POSTGRES_USER,
-      password: POSTGRES_PASSWORD,
+        host: POSTGRES_HOST,
+        database: POSTGRES_DB,
+        user: POSTGRES_USER,
+        password: POSTGRES_PASSWORD,
     });
-  }
-  
-  if (ENV === 'TEST') {
-    client = new Pool({
-      host: POSTGRES_HOST,
-      database: POSTGRES_TEST_DB,
-      user: POSTGRES_USER,
-      password: POSTGRES_PASSWORD,
-    });
-  }
+}
 
-  export default client;
+if (ENV === 'TEST') {
+    client = new Pool({
+        host: POSTGRES_HOST,
+        database: POSTGRES_TEST_DB,
+        user: POSTGRES_USER,
+        password: POSTGRES_PASSWORD,
+    });
+}
+
+if (ENV === 'DEV') {
+    client = new Pool({
+        host: POSTGRES_HOST,
+        database: POSTGRES_TEST_DB,
+        user: POSTGRES_USER,
+        password: POSTGRES_PASSWORD,
+    });
+}
+
+
+export default client;
