ebf50bd4bacc2647d09743aa9f5607e3ea70faa9
Toan Nguyen <nguyenvantoan263@gmail.com>
2023-10-22 20:53:23 +0800

    Update server file

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,25 @@
-import express, { Request, Response } from 'express'
-import bodyParser from 'body-parser'
+import express, { Application, Request, Response } from 'express';
+import bodyParser from 'body-parser';
 
-const app: express.Application = express()
-const address: string = "0.0.0.0:3000"
+import userRoutes from './handlers/user';
+import productRoutes from './handlers/product';
+import orderRoutes from './handlers/order_controller';
 
-app.use(bodyParser.json())
+const app: express.Application = express();
+let port = 3000;
+const address: string = "127.0.0.1:${port}";
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
 
 app.get('/', function (req: Request, res: Response) {
-    res.send('Hello World!')
+    res.send('Hello! This is Storefront Application');
 })
 
+userRoutes(app);
+productRoutes(app);
+orderRoutes(app);
+
 app.listen(3000, function () {
-    console.log(`starting app on: ${address}`)
+    console.log(`starting app on: ${address}`);
 })
